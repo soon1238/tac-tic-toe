1a6ee89e94cb5022f58c8de0835d038d65b772c3
Kosala Yapa <kosalayapa@gmail.com>
2018-10-07 17:35:46 +0800

    Changed env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,9 @@
 export const environment = {
   production: false,
   pusher: {
+    appId: '615946',
     key: 'e31eef0acf7f58091bd0',
+    secret: '5278a248ec7028d464ac',
     endpoint: 'http://localhost:7200/pusher/auth/',
     cluster: 'ap1'
   }
